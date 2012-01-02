e32f74059178b34450c29686c1a5a987802e520c
croxis <croxis@yahoo.com>
2012-01-02 00:05:50 -0800

    Some temp debug code to catch a NPE

--- src/main/java/net/croxis/plugins/research/RInventoryListener.java
+++ src/main/java/net/croxis/plugins/research/RInventoryListener.java
@@ -6,9 +6,13 @@ import java.util.HashSet;
 
 public class RInventoryListener extends InventoryListener{
 	public void onInventoryCraft(InventoryCraftEvent event){
-		if (event.getPlayer().hasPermission("research"))
+		if (event.getPlayer().hasPermission("research")){
+			RPlayer tmp = TechManager.players.get(event.getPlayer());
+			event.getResult().getTypeId();
+			event.getPlayer().hasPermission("research");
 			if(TechManager.players.get(event.getPlayer()).cantCraft.contains(event.getResult().getTypeId()) && event.getPlayer().hasPermission("research"))
 				event.setCancelled(true);
+		}
 	}
 
 }
