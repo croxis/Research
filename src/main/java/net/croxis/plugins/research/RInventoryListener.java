972c4c33dca8a0fd1161d8314fed9164dc47966f
croxis <croxis@yahoo.com>
2012-06-16 08:36:55 -0700

    Removed spout dependency

--- src/main/java/net/croxis/plugins/research/RInventoryListener.java
+++ src/main/java/net/croxis/plugins/research/RInventoryListener.java
@@ -2,16 +2,16 @@ package net.croxis.plugins.research;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.getspout.spoutapi.event.inventory.InventoryCraftEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
 import java.util.HashSet;
 
 @SuppressWarnings("unused")
 public class RInventoryListener implements Listener{
 	@EventHandler
-	public void onInventoryCraft(InventoryCraftEvent event){
-		if (event.getPlayer().hasPermission("research")){
-			//event.getResult().getTypeId();
-			if(TechManager.players.get(event.getPlayer()).cantCraft.contains(event.getResult().getTypeId()) && event.getPlayer().hasPermission("research"))
+	public void onInventoryCraft(CraftItemEvent event){
+		if (event.getWhoClicked().hasPermission("research")){
+			//if(TechManager.players.get(event.getWhoClicked()).cantCraft.contains(event.getResult().getTypeId()) && event.getPlayer().hasPermission("research"))
+			if(TechManager.players.get(event.getWhoClicked()).cantCraft.contains(event.getRecipe().getResult().getTypeId()))
 				event.setCancelled(true);
 		}
 	}
