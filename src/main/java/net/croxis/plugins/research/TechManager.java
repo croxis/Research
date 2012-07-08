11f6d249eb2a6733f3970cee63b31285a7e76d73
croxis <croxis@yahoo.com>
2012-07-08 08:23:12 -0700

    Fixed npe if player doesn't not know techs

--- src/main/java/net/croxis/plugins/research/TechManager.java
+++ src/main/java/net/croxis/plugins/research/TechManager.java
@@ -75,10 +75,10 @@ public class TechManager {
 		PermissionAttachment att = player.addAttachment(plugin);
 		for(String techName : sqlplayer.getResearched().split(",")){
 			Tech tech = techs.get(techName);
-			if (tech.permissions != null)
-				for (String perm : tech.permissions){
-					att.setPermission(perm, false);
-				}
+			if (tech != null)
+				if (tech.permissions != null)
+					for (String perm : tech.permissions)
+						att.setPermission(perm, false);
 		}
 		player.recalculatePermissions();
 		players.remove(player);
